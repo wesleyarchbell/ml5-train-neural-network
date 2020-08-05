161dba9ccf6047ba3397698d35a168ba7a9945c9
Wesley Archbell <wesleyarchbell@gmail.com>
2020-08-05 16:50:15 +1000

    Add neural network training functionality

--- sketch.js
+++ sketch.js
@@ -1,38 +1,84 @@
 let model;
 let targetLabel;
+let state = 'collection';
+
+let mouseXPressed;
+let mouseYPressed;
 
 function setup() {
-    createCanvas(400, 400);
+
+    createCanvas(400, 400);       
 
     let options = {
         inputs: ['x', 'y'],
         outputs: ['label'],
-        task: 'classification'            
+        task: 'classification',
+        debug: true       
     };
     model = ml5.neuralNetwork(options);
 }
 
 function keyPressed() {
+    if ('t' === key) {
+        state = 'training';
+        console.log('Starting training..');
+        model.normalizeData();
+        let options = {
+            epochs: 200        
+        };
+        model.train(options, whileTraining, finishedTraining);
+    }
     targetLabel = key.toUpperCase();
+}
+
+function whileTraining(epoch, loss) {
+    console.log(epoch);
+}
+
+function finishedTraining() {
+    state = 'prediction';
+    console.log('Finished training model');
 }
 
 function mousePressed() {
 
+    mouseXPressed = mouseX;
+    mouseYPressed = mouseY;
+
+
     let inputs = {
         x: mouseX,
         y: mouseY
-    }
+    }  
 
-    let target = {
-        label: targetLabel
+    if (state === 'collection') {       
+        let target = {
+            label: targetLabel
+        }
+        model.addData(inputs, target);
+        stroke(0);
+        noFill();
+        ellipse(mouseX, mouseY, 24);
+        fill(0);
+        noStroke();
+        textAlign(CENTER, CENTER);
+        text(targetLabel, mouseX, mouseY);       
+
+    } else if (state === 'prediction') {
+        model.classify(inputs, gotResults);
     }
-    model.addData(inputs, target);
-    
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+        return;
+    }   
     stroke(0);
-    noFill();
-    ellipse(mouseX, mouseY, 24);
+    fill(0, 0, 255, 100);
+    ellipse(mouseXPressed, mouseYPressed, 24);
     fill(0);
     noStroke();
     textAlign(CENTER, CENTER);
-    text(targetLabel, mouseX, mouseY);
+    text(results[0].label, mouseX, mouseY);
 }
